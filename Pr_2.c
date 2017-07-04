6da73c27d53ae2103dd86c45fc704f35aa40c62a
Abhishek-Singha <abhisheksingha626@gmail.com>
2017-07-04 01:40:24 -0700

    How operate variable by using int command.

---
+++ Pr_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{float a,b,c,d,sum,per,ave;
+a=70,b=50,c=95,d=59;
+sum=a+b+c+d;
+ave=sum/4;
+per=sum/4;
+printf ("Total:%f",sum);
+printf ("\n\----------------------\n");
+printf ("fort:%f",ave);
+printf ("\n\----------------------\n");
+printf ("Portion:%f",per);
+printf ("\n\----------------------\n");
+}
